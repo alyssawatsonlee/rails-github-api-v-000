8e59175ae4d695c4fe709e3ad0b2b86dde283300
Alyssa Watson <alyssa.s.watson@gmail.com>
2018-10-24 20:47:51 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,6 +1,6 @@
 class RepositoriesController < ApplicationController
 
-  
+
 def index
   user = Faraday.get 'https://api.github.com/user' do |req|
       req.headers['Authorization'] = 'token ' + session[:token]
