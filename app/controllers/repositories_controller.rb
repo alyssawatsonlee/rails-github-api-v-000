00d2679e47f33a484bb60d3609501ec00c69533d
Alyssa Watson <alyssa.s.watson@gmail.com>
2018-10-24 20:46:11 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,11 +1,11 @@
 class RepositoriesController < ApplicationController
-  
+
   user = Faraday.get 'https://api.github.com/user' do |req|
       req.headers['Authorization'] = 'token ' + session[:token]
       req.headers['Accept'] = 'application/json'
     end
      @user_data = JSON.parse(user.body)
-    
+
     repos = Faraday.get 'https://api.github.com/user/repos' do |req|
       req.headers['Authorization'] = 'token ' + session[:token]
       req.headers['Accept'] = 'application/json'
